721f98837001a1ffdee2bfc64eebe5d2e94dbd93
maxmcdonald014@gmail.com <maxmcdonald014@gmail.com>
2021-06-18 22:03:34 -0400

    test

---
+++ src/bot.js
@@ -0,0 +1,10 @@
+import { config } from 'dotenv';
+config({path: '.env' });
+
+import { Client } from 'discord.js';
+
+const bot = new Client();
+
+bot.on('ready', () => console.log(`${bot.user.username} is online`));
+
+bot.login(process.env.BOT_TOKEN);
