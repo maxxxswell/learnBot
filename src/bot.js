c7869bf061cf833c1b92d75009c4e5de08df9bbe
maxmcdonald014@gmail.com <maxmcdonald014@gmail.com>
2021-06-19 12:02:02 -0400

    test

--- src/bot.js
+++ src/bot.js
@@ -5,8 +5,6 @@ import { Client } from 'discord.js';
 
 const bot = new Client();
 
-llll
-
 bot.on('ready', () => console.log(`${bot.user.username} is online`));
 
 bot.login(process.env.BOT_TOKEN);
