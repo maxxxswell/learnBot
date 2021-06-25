450fb9f08e3e6eeab5d129b06c0ab4d234cd69a6
maxmcdonald014@gmail.com <maxmcdonald014@gmail.com>
2021-06-24 23:03:58 -0400

    commandhandler

--- src/handlers/command-handler.js
+++ src/handlers/command-handler.js
@@ -12,15 +12,20 @@ export class CommandHandler {
 
             this.commands.set(command.name, command);
         }
+        console.log(`${fileNames.length - 1} commands loaded.`);
     }
 
     async handle(prefix, msg) {
-        const words = msg.content // .help moderation 2
-            .slice(prefix.length) // help moderation 2
-            .split(' '); // ['.help', 'moderation', '2']
+        try {
+            const words = msg.content // .help moderation 2
+                .slice(prefix.length) // help moderation 2
+                .split(' '); // ['.help', 'moderation', '2']
 
-        await this.commands
-            .get(words[0])
-            ?.execute(msg, ...words.slice(1)); // ['moderation', '2']
+             await this.commands
+                .get(words[0])
+                ?.execute(msg, ...words.slice(1)); // ['moderation', '2']
+        } catch (error) {
+            await msg.reply(` ⚠ ${error.message}`);
+        }
     }
 }
